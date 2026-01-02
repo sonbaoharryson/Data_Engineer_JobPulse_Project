91e026b927787221d46e04857c4057df1195c4ec
sonbao0901 <phanb688@gmail.com>
2026-01-02 13:12:17 +0700

    change file location

--- airflow/scripts/crawl_scripts/test/test_crawl_it_viec.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from crawl_job.it_viec import scrape_jobs_it_viec
-
-data = scrape_jobs_it_viec("https://itviec.com/it-jobs/data-analyst")
-print(data)
--- airflow/scripts/crawl_scripts/test/test_db_conn.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from crawl_job.crawler import JobDataIngestion
-
-conn = JobDataIngestion()._create_db_connection()
-print(conn)
