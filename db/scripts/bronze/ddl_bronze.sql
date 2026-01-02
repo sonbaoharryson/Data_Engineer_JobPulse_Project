a3b1d4dc6728575617cf05fa33537dc0692eae45
sonbao0901 <phanb688@gmail.com>
2026-01-02 13:12:50 +0700

    add fields for itviec and topcv table

--- db/scripts/bronze/ddl_bronze.sql
+++ db/scripts/bronze/ddl_bronze.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS bronze.topcv_data_job (
+CREATE TABLE IF NOT EXISTS job_db_sm4x.bronze.topcv_data_job (
     id SERIAL PRIMARY KEY,
     title VARCHAR(200) NOT NULL,
     company VARCHAR(150) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS bronze.topcv_data_job (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS bronze.itviec_data_job (
+CREATE TABLE IF NOT EXISTS job_db_sm4x.bronze.itviec_data_job (
     id SERIAL PRIMARY KEY,
     title VARCHAR(200) NOT NULL,
     company VARCHAR(150) NOT NULL,
@@ -24,14 +24,54 @@ CREATE TABLE IF NOT EXISTS bronze.itviec_data_job (
 );
 
 -- Add new columns to bronze.itviec_data_job
-alter table bronze.topcv_data_job
-add column descriptions text,
-add column requirements text,
-add column experience varchar(100),
-add column education varchar(100),
-add column type_of_work varchar(50);
--- DDL
--- TRUNCATE TABLE discord_job_db.bronze.itviec_data_job;
--- TRUNCATE TABLE discord_job_db.bronze.topcv_data_job;
--- ALTER SEQUENCE discord_job_db.bronze.topcv_data_job_id_seq RESTART WITH 1;
--- ALTER SEQUENCE discord_job_db.bronze.itviec_data_job_id_seq RESTART WITH 1;
+ALTER TABLE job_db_sm4x.bronze.itviec_data_job
+ADD COLUMN posted_to_discord BOOLEAN DEFAULT FALSE;
+
+
+ALTER TABLE job_db_sm4x.bronze.topcv_data_job
+ADD COLUMN descriptions TEXT,
+ADD COLUMN requirements TEXT,
+ADD COLUMN experience VARCHAR(100),
+ADD COLUMN education VARCHAR(100),
+ADD COLUMN type_of_work VARCHAR(50),
+ADD COLUMN posted_to_discord BOOLEAN DEFAULT FALSE;
+
+-- FIXING SEQUENCE ID COLUMN
+/* TRUNCATE TABLE discord_job_db.bronze.itviec_data_job;
+TRUNCATE TABLE discord_job_db.bronze.topcv_data_job;
+ALTER SEQUENCE discord_job_db.bronze.topcv_data_job_id_seq RESTART WITH 1;
+ALTER SEQUENCE discord_job_db.bronze.itviec_data_job_id_seq RESTART WITH 1; */
+
+-- HANDLING DUPLICATE DATA DUE TO UNCLEANSE URLS
+/* create table temp_top_cv_data as
+with cleanse_url as(
+	select *, trim(SPLIT_PART(url, '?ta_source', 1)) new_url
+	from bronze.topcv_data_job
+), rn_url as(
+	select *,
+		row_number() over (partition by new_url) rn
+	from cleanse_url
+), dedup as(
+	select
+		id,
+		title,
+		company,
+		logo,
+		new_url url,
+		location,
+		salary,
+		created_at,
+		descriptions,
+		requirements,
+		experience,
+		education,
+		type_of_work,
+		posted_to_discord
+	from rn_url
+	where rn = 1
+)
+select * from dedup;
+truncate table bronze.topcv_data_job;
+insert into bronze.topcv_data_job
+select * from temp_top_cv_data;
+drop table temp_top_cv_data; */
