dee149c2ad5d4af7becf62f1b92c41ce5ec3cfdf
sonbao0901 <phanb688@gmail.com>
2026-01-02 13:20:42 +0700

    create dag for itviec pipeline

--- airflow/dags/job_itviec_pipeline.py
+++ airflow/dags/job_itviec_pipeline.py
@@ -1,12 +1,20 @@
 import os
 import sys
-sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from airflow.decorators import dag, task
 from datetime import datetime, timedelta
 from scripts.crawl_scripts.crawl_job.crawler import Crawler
-from scripts.insert_data_bronze import insert_itviec_jobs
+from scripts.utils.insert_data_bronze import insert_itviec_jobs
 from scripts.utils.load_crawl_source import load_crawl_sources
+from scripts.utils.sender import query_unposted_jobs, mark_jobs_as_posted, send_job_alerts
+import logging
 
+logger = logging.getLogger(__name__)
+if not logger.handlers:
+    logging.basicConfig(level=logging.INFO)
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 #Define DAG
 default_args = {
     'owner': 'sonbao',
@@ -34,24 +42,41 @@ def itviec_pipeline():
     @task
     def scrape_itviec_job(sources: dict):
         crawler = Crawler("itviec")
+        total_data_job_itviec = []
         for source, url in sources.items():
-            print(f"Processing source: {source} with URL: {url}")
+            logger.info(f"Processing source: {source} with URL: {url}")
             try:
                 itviec_jobs = crawler.crawler(url)
                 if itviec_jobs:
-                    print(f"Successfully scraped {len(itviec_jobs)} jobs from IT Viec")
+                    logger.info(f"Successfully scraped {len(itviec_jobs)} jobs from IT Viec")
             except Exception as e:
-                print(f"Error scraping IT Viec: {e}")
+                logger.error(f"Error scraping IT Viec: {e}")
+            total_data_job_itviec += itviec_jobs
+        return total_data_job_itviec if total_data_job_itviec else None
 
     @task
     def insert_jobs(data):
         if not data:
-            print("No IT Viec jobs to insert")
+            logger.info("No IT Viec jobs to insert")
             return
         insert_itviec_jobs(data)
 
+    @task
+    def post_job_to_discord():
+        query_data = query_unposted_jobs(table_name='itviec_data_job')
+        jobs, urls = query_data
+        logger.info(f"Found {len(jobs)} new IT Viec jobs to post to Discord")
+        if not jobs:
+            logger.info('No new jobs from IT Viec to post to Discord')
+            return
+        try:
+            send_job_alerts(jobs, DISCORD_TOKEN, DISCORD_CHANNEL_ID)
+            mark_jobs_as_posted(table_name='itviec_data_job', job_urls=urls)
+        except Exception as e:
+            logger.error(f"Error sending job alerts to Discord: {e}")
+
     itviec_sources=load_itviec_url()
     job_data=scrape_itviec_job(itviec_sources)
-    insert_jobs(job_data)
+    insert_jobs(job_data) >> post_job_to_discord()
 
 dag = itviec_pipeline()
