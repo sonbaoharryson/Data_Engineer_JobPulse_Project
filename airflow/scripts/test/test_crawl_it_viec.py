833add32d2632c59474c68b6de400658070f5ec6
sonbao0901 <phanb688@gmail.com>
2026-01-04 14:41:48 +0700

    test script for crawl sources

--- airflow/scripts/test/test_crawl_it_viec.py
+++ airflow/scripts/test/test_crawl_it_viec.py
@@ -2,6 +2,9 @@ import os
 import sys
 sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from crawl_scripts.crawl_job.it_viec import ITViecScraper
-
+import pandas as pd
 data = ITViecScraper(headless=False).scrape_jobs('https://itviec.com/it-jobs/data-science')
 print(data)
+
+df = pd.DataFrame(data)
+df.head(20)
--- airflow/scripts/test/test_crawl_topcv.py
+++ airflow/scripts/test/test_crawl_topcv.py
@@ -3,5 +3,5 @@ import sys
 sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from crawl_scripts.crawl_job.topcv import TopCVScraper
 
-data = TopCVScraper(headless=False).scrape_jobs("https://www.topcv.vn/tim-viec-lam-data-analyst")
+data = TopCVScraper(headless=False).scrape_jobs("https://www.topcv.vn/tim-viec-lam-data-scientist")
 print(data)
