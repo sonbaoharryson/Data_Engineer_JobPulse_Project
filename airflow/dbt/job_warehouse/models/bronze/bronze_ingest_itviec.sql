86339817b3a9f7fcd6adfe55a3c2484c0309a961
sonbao0901 <phanb688@gmail.com>
2026-01-07 17:07:31 +0700

    create model for bronze layer for source itviec

---
+++ airflow/dbt/job_warehouse/models/bronze/bronze_ingest_itviec.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table', tags=['bronze_layer', 'basic_cleanse', 'itviec'])}}
+
+SELECT
+    id,
+    {{ initcap_and_trim('title') }} AS title,
+    {{ upper_and_trim('company') }} AS company,
+    {{ lower_and_trim('logo_url') }} AS logo_url,
+    {{ lower_and_trim('url') }} AS url,
+    {{ initcap_and_trim('working_location') }} AS working_location,
+    {{ initcap_and_trim('work_model') }} AS work_model,
+    {{ initcap_and_trim('tags') }} AS tags,
+    {{ lower_and_trim('descriptions') }} AS descriptions,
+    {{ lower_and_trim('requirements_and_experiences') }} AS requirements_and_experiences,
+    posted_to_discord,
+    created_at
+from {{ source('job_raw', 'itviec_data_job') }}
