94425b5961fbb0cbfb860e5b2f5e47138fcc6e2b
sonbao0901 <phanb688@gmail.com>
2026-01-09 16:55:35 +0700

    create task function for airflow

---
+++ airflow/tasks/process_tasks.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import logging
+sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from typing import List, Dict
+from scripts.crawl_scripts.crawl_job.crawler import Crawler
+from scripts.utils.load_crawl_source import load_crawl_sources
+from scripts.utils.insert_data_staging import insert_itviec_jobs, insert_topcv_jobs
+from scripts.utils.sender import query_unposted_jobs, mark_jobs_as_posted, send_job_alerts
+
+logger = logging.getLogger(__name__)
+if not logger.handlers:
+    logging.basicConfig(level=logging.INFO)
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
+
+def load_crawl_sources_url(source_crawl:str):
+    if source_crawl=='itviec':
+        data = load_crawl_sources(file_name='source_itviec.json')
+    elif source_crawl=='topcv':
+        data = load_crawl_sources(file_name='source_topcv.json')
+    return data
+
+def scrape_source_job(sources: dict, source_crawl:str):
+    crawler = Crawler(source_crawl)
+    total_data_job = []
+    for source, url in sources.items():
+        logger.info(f"Processing source: {source} with URL: {url}")
+        try:
+            dict_jobs = crawler.crawler(url)
+            if dict_jobs:
+                logger.info(f"Successfully scraped {len(dict_jobs)} jobs from {source_crawl}")
+        except Exception as e:
+            logger.error(f"Error scraping {source_crawl}: {e}")
+        total_data_job += dict_jobs
+    return total_data_job if total_data_job else None
+
+def insert_jobs_to_staging_layer(data:List[Dict], source_crawl:str):
+    if not data:
+        logger.info(f"No {source_crawl} jobs to insert")
+        return
+
+    if source_crawl=='itviec':
+        insert_itviec_jobs(data)
+    elif source_crawl=='topcv':
+        insert_topcv_jobs(data)
+
+def post_job_to_discord(crawl_source:str):
+    query_data = query_unposted_jobs(table_name='itviec_data_job') if crawl_source=='itviec' else query_unposted_jobs(table_name='topcv_data_job')
+    jobs, urls = query_data
+    logger.info(f"Found {len(jobs)} new {crawl_source} jobs to post to Discord")
+    if not jobs:
+        logger.info(f'No new jobs from {crawl_source} to post to Discord')
+        return
+    try:
+        send_job_alerts(jobs, DISCORD_TOKEN, DISCORD_CHANNEL_ID)
+        mark_jobs_as_posted(table_name='itviec_data_job', job_urls=urls) if crawl_source=='itviec' else mark_jobs_as_posted(table_name='topcv_data_job', job_urls=urls)
+    except Exception as e:
+        logger.error(f"Error sending job alerts to Discord: {e}")
---
+++ airflow/tasks/tasks_group.py
@@ -0,0 +1,50 @@
+from airflow.decorators import task, task_group
+from tasks.process_tasks import load_crawl_sources_url, scrape_source_job, insert_jobs_to_staging_layer, post_job_to_discord
+
+@task_group
+def itviec_pipeline():
+    @task
+    def load_itviec_url():
+        return load_crawl_sources_url(source_crawl="itviec")
+
+    @task
+    def scrape_itviec_job(sources: dict):
+        return scrape_source_job(sources=sources, source_crawl="itviec")
+
+    @task
+    def insert_jobs_itviec(data):
+        insert_jobs_to_staging_layer(data=data, source_crawl="itviec")
+
+    @task
+    def post_job_to_discord_itviec():
+        post_job_to_discord(crawl_source="itviec")
+
+    source_crawl = load_itviec_url()
+    scrapped_data = scrape_itviec_job(source_crawl)
+    insert_data = insert_jobs_itviec(scrapped_data)
+    discord_post_job = post_job_to_discord_itviec()
+    insert_data >> discord_post_job
+
+@task_group
+def topcv_pipeline():
+    @task
+    def load_topcv_url():
+        return load_crawl_sources_url(source_crawl="topcv")
+
+    @task
+    def scrape_topcv_job(sources: dict):
+        return scrape_source_job(sources=sources, source_crawl="topcv")
+
+    @task
+    def insert_jobs_topcv(data):
+        insert_jobs_to_staging_layer(data=data, source_crawl="topcv")
+
+    @task
+    def post_job_to_discord_topcv():
+        post_job_to_discord(crawl_source="topcv")
+
+    source_crawl = load_topcv_url()
+    scrapped_data = scrape_topcv_job(source_crawl)
+    insert_data = insert_jobs_topcv(scrapped_data)
+    discord_post_job = post_job_to_discord_topcv()
+    insert_data >> discord_post_job
