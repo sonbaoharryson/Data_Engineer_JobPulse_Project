2ddc0ca78ae5970b67a8bece9ae0b5de95e4bc6a
sonbao0901 <phanb688@gmail.com>
2026-01-09 16:56:14 +0700

    refactor itviec and topcv dag

--- airflow/dags/job_itviec_pipeline.py
+++ airflow/dags/job_itviec_pipeline.py
@@ -1,20 +1,15 @@
 import os
+import logging
 import sys
 sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from airflow.decorators import dag, task
 from datetime import datetime, timedelta
-from scripts.crawl_scripts.crawl_job.crawler import Crawler
-from scripts.utils.insert_data_staging import insert_itviec_jobs
-from scripts.utils.load_crawl_source import load_crawl_sources
-from scripts.utils.sender import query_unposted_jobs, mark_jobs_as_posted, send_job_alerts
-import logging
+from tasks.process_tasks import load_crawl_sources_url, scrape_source_job, insert_jobs_to_staging_layer, post_job_to_discord
 
 logger = logging.getLogger(__name__)
 if not logger.handlers:
     logging.basicConfig(level=logging.INFO)
 
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 #Define DAG
 default_args = {
     'owner': 'sonbao',
@@ -27,7 +22,7 @@ default_args = {
 @dag(
     dag_id='itviec_data_pipeline',
     default_args=default_args,
-    schedule_interval=timedelta(days=15),
+    schedule='@daily',
     catchup=False,
     tags=['itviec_pipeline']
 )
@@ -36,47 +31,28 @@ def itviec_pipeline():
 
     @task
     def load_itviec_url():
-        data = load_crawl_sources(file_name='source_itviec.json')
-        return data
+        return load_crawl_sources_url(source_crawl='itviec')
 
     @task
     def scrape_itviec_job(sources: dict):
-        crawler = Crawler("itviec")
-        total_data_job_itviec = []
-        for source, url in sources.items():
-            logger.info(f"Processing source: {source} with URL: {url}")
-            try:
-                itviec_jobs = crawler.crawler(url)
-                if itviec_jobs:
-                    logger.info(f"Successfully scraped {len(itviec_jobs)} jobs from IT Viec")
-            except Exception as e:
-                logger.error(f"Error scraping IT Viec: {e}")
-            total_data_job_itviec += itviec_jobs
-        return total_data_job_itviec if total_data_job_itviec else None
+        return scrape_source_job(sources=sources, source_crawl='itviec')
 
     @task
-    def insert_jobs(data):
-        if not data:
-            logger.info("No IT Viec jobs to insert")
-            return
-        insert_itviec_jobs(data)
+    def insert_jobs_itviec(data):
+        insert_jobs_to_staging_layer(data=data, source_crawl='itviec')
 
     @task
-    def post_job_to_discord():
-        query_data = query_unposted_jobs(table_name='itviec_data_job')
-        jobs, urls = query_data
-        logger.info(f"Found {len(jobs)} new IT Viec jobs to post to Discord")
-        if not jobs:
-            logger.info('No new jobs from IT Viec to post to Discord')
-            return
-        try:
-            send_job_alerts(jobs, DISCORD_TOKEN, DISCORD_CHANNEL_ID)
-            mark_jobs_as_posted(table_name='itviec_data_job', job_urls=urls)
-        except Exception as e:
-            logger.error(f"Error sending job alerts to Discord: {e}")
+    def post_job_to_discord_itviec():
+        post_job_to_discord(crawl_source='itviec')
 
-    itviec_sources=load_itviec_url()
-    job_data=scrape_itviec_job(itviec_sources)
-    insert_jobs(job_data) >> post_job_to_discord()
+    itviec_sources = load_itviec_url()
+    job_data = scrape_itviec_job(itviec_sources)
+    insert = insert_jobs_itviec(job_data)
+    discord = post_job_to_discord_itviec()
+
+    insert >> discord
+
+    # insert >> bronze
+    # bronze >> [silver, discord] >> gold
 
 dag = itviec_pipeline()
--- airflow/dags/job_topcv_pipeline.py
+++ airflow/dags/job_topcv_pipeline.py
@@ -1,20 +1,15 @@
 import os
+import logging
 import sys
 sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from airflow.decorators import dag, task
 from datetime import datetime, timedelta
-from scripts.crawl_scripts.crawl_job.crawler import Crawler
-from scripts.utils.insert_data_staging import insert_topcv_jobs
-from scripts.utils.load_crawl_source import load_crawl_sources
-from scripts.utils.sender import query_unposted_jobs, mark_jobs_as_posted, send_job_alerts
-import logging
+from tasks.process_tasks import load_crawl_sources_url, scrape_source_job, insert_jobs_to_staging_layer, post_job_to_discord
 
 logger = logging.getLogger(__name__)
 if not logger.handlers:
     logging.basicConfig(level=logging.INFO)
 
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 
 #Define DAG
 default_args = {
@@ -37,47 +32,31 @@ def topcv_pipeline():
 
     @task
     def load_topcv_url():
-        data = load_crawl_sources(file_name='source_topcv.json')
-        return data
+        return load_crawl_sources_url(source_crawl='topcv')
 
     @task
     def scrape_topcv_job(sources: dict):
-        crawler = Crawler("topcv")
-        total_data_job_topcv = []
-        for source, url in sources.items():
-            print(f"Processing source: {source} with URL: {url}")
-            try:
-                topcv_jobs = crawler.crawler(url)
-                if topcv_jobs:
-                    print(f"Successfully scraped {len(topcv_jobs)} jobs from TopCV")
-            except Exception as e:
-                print(f"Error scraping TopCV: {e}")
-            total_data_job_topcv += topcv_jobs
-        return total_data_job_topcv if total_data_job_topcv else None
+        return scrape_source_job(sources=sources, source_crawl='topcv')
+
 
     @task
-    def insert_jobs(data):
-        if not data:
-            print("No TopCV jobs to insert")
-            return
-        insert_topcv_jobs(data)
+    def insert_jobs_topcv(data):
+        insert_jobs_to_staging_layer(data=data, source_crawl='topcv')
+
 
     @task
-    def post_job_to_discord():
-        query_data = query_unposted_jobs(table_name='topcv_data_job')
-        jobs, urls = query_data
-        logger.info(f"Found {len(jobs)} new TopCV jobs to post to Discord")
-        if not jobs:
-            logger.info('No new jobs from TopCV to post to Discord')
-            return
-        try:
-            send_job_alerts(jobs, DISCORD_TOKEN, DISCORD_CHANNEL_ID)
-            mark_jobs_as_posted(table_name='topcv_data_job', job_urls=urls)
-        except Exception as e:
-            logger.error(f"Error sending job alerts to Discord: {e}")
+    def post_job_to_discord_topcv():
+        post_job_to_discord(crawl_source='topcv')
 
-    topcv_sources=load_topcv_url()
-    job_data=scrape_topcv_job(topcv_sources)
-    insert_jobs(job_data) >> post_job_to_discord()
+
+    topcv_sources = load_topcv_url()
+    job_data = scrape_topcv_job(topcv_sources)
+    insert = insert_jobs_topcv(job_data)
+    discord = post_job_to_discord_topcv()
+
+    insert >> discord
+
+    # insert >> bronze
+    # bronze >> [silver, discord] >> gold
 
 dag = topcv_pipeline()
