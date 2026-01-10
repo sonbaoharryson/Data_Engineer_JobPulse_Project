8d117bf2708d912084161843644862cf64b82dfb
sonbao0901 <phanb688@gmail.com>
2026-01-10 16:17:16 +0700

    sample dbt dag using Bash

--- airflow/dags/sample_dbt_dag.py
+++ airflow/dags/sample_dbt_dag.py
@@ -21,25 +21,6 @@ def sample_dbt_call():
     def ingest_bronze():
         return f'dbt run --select bronze --project-dir {PROJECT_DIR} --profiles-dir {PROFILE_DIR}'
 
-    @task.bash
-    def ingest_silver():
-        return f'dbt run --select silver --project-dir {PROJECT_DIR} --profiles-dir {PROFILE_DIR}'
-
-    # @task.bash
-    # def process_bronze_wh_layer():
-    #     logger.info('Starting process data to bronze layer!!!')
-    #     return f'dbt run --select bronze --project-dir {PROJECT_DIR} --profiles-dir {PROFILE_DIR}'
-
-    # @task.bash
-    # def process_silver_wh_layer():
-    #     logger.info('Starting process data to silver layer!!!')
-    #     return f'dbt run --select silver --project-dir {PROJECT_DIR} --profiles-dir {PROFILE_DIR}'
-
-    # @task.bash
-    # def process_gold_wh_layer():
-    #     logger.info('Starting process data to gold layer!!!')
-    #     return f'dbt run --select gold --project-dir {PROJECT_DIR} --profiles-dir {PROFILE_DIR}'
-
-    check_dbt_conn() >> ingest_bronze() >> ingest_silver()
+    check_dbt_conn() >> ingest_bronze()
 
 dag = sample_dbt_call()
