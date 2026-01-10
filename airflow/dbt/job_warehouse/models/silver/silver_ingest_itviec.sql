64822e72be489b30ce3aeb2933331b4ac7aab4a1
sonbao0901 <phanb688@gmail.com>
2026-01-10 16:18:12 +0700

    create model in silver layer

--- airflow/dbt/job_warehouse/models/silver/silver_ingest_itviec.sql
+++ airflow/dbt/job_warehouse/models/silver/silver_ingest_itviec.sql
@@ -1,9 +1,7 @@
 {{ config(materialized='table', tags=['silver_layer', 'enrich_education_column', 'itviec'])}}
-with temp as(
+
 SELECT
     *,
     {{ least_level_of_education('requirements_and_experiences') }} AS least_level_of_education,
     {{ extract_experience('requirements_and_experiences') }} AS exp
 FROM {{ source('bronze_layer', 'bronze_ingest_itviec') }}
-)
-select * from temp where exp <> 'Unknown'
--- airflow/dbt/job_warehouse/models/silver/silver_ingest_topcv.sql
+++ airflow/dbt/job_warehouse/models/silver/silver_ingest_topcv.sql
@@ -1,9 +1,7 @@
 {{ config(materialized='table', tags=['silver_layer', 'enrich_education_column', 'topcv'])}}
-with temp as(
+
 SELECT
     *,
     {{ least_level_of_education('level_of_education') }} AS least_level_of_education,
     {{ extract_experience('experiences') }} AS exp
 FROM {{ source('bronze_layer', 'bronze_ingest_topcv') }}
-)
-select * from temp where exp <> 'Unknown'
