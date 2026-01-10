bae7810d1fcb6b54dc3a7ef074589938c364abfe
sonbao0901 <phanb688@gmail.com>
2026-01-10 16:19:12 +0700

    correct schema name - return total job sent and failed

--- airflow/scripts/utils/sender.py
+++ airflow/scripts/utils/sender.py
@@ -11,10 +11,10 @@ if not logger.handlers:
 
 def query_unposted_jobs(table_name: str):
     engine = DBConnection().engine
-    salary_col = "salary" if table_name == 'topcv_data_job' else "'Sign-in ITViec for details.'"
+    salary_col = "salary" if table_name == 'bronze_ingest_topcv' else "'Sign-in ITViec for details.'"
     query = text(f"""
         SELECT title, url, logo_url as logo, working_location as location, {salary_col} as salary
-        FROM job_db_sm4x.staging.{table_name}
+        FROM job_db_sm4x.job_wh_bronze.{table_name}
         WHERE posted_to_discord = FALSE
     """)
     with engine.connect() as conn:
@@ -32,7 +32,7 @@ def mark_jobs_as_posted(table_name: str, job_urls: list):
 
     engine = DBConnection().engine
     query = text(f"""
-        UPDATE job_db_sm4x.staging.{table_name}
+        UPDATE job_db_sm4x.job_wh_bronze.{table_name}
         SET posted_to_discord = TRUE
         WHERE url = ANY(:urls)
     """)
@@ -62,24 +62,36 @@ async def send_jobs(jobs: List[dict], token: str, channel_id: int, throttle: flo
             pass
         return
 
+    success_job_send = 0
+    fail_job_send = 0
     try:
         for job in jobs:
             try:
                 embed = job_to_embed(job)
                 await channel.send(embed=embed)
+                logger.info(f'Succeeded to send job: {job}')
+                success_job_send+=1
                 if throttle and throttle > 0:
                     await asyncio.sleep(throttle)
             except Exception:
                 logger.exception("Failed to send one job message")
+                fail_job_send+=1
     finally:
         try:
             await client.close()
         except Exception:
             logger.exception("Error closing Discord client")
+    
+    return success_job_send, fail_job_send
 
 def send_job_alerts(jobs, token: str, channel_id: int):
     if not jobs:
         logger.info("No jobs to send")
         return
 
-    asyncio.run(send_jobs(jobs, token, channel_id))
+    job_sent_status = asyncio.run(send_jobs(jobs, token, channel_id))
+    
+    total_job_success_sent = job_sent_status[0]
+    total_job_fail_sent = job_sent_status[1]
+    
+    return total_job_success_sent, total_job_fail_sent
