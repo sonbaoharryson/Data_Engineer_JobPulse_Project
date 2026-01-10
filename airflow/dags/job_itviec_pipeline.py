14e054953541e54f03e5e3e0e0685d8f01db2a08
sonbao0901 <phanb688@gmail.com>
2026-01-10 16:16:38 +0700

    add callback audit to itviec pipeline

--- airflow/dags/job_itviec_pipeline.py
+++ airflow/dags/job_itviec_pipeline.py
@@ -4,7 +4,8 @@ import sys
 sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from airflow.decorators import dag, task
 from datetime import datetime, timedelta
-from tasks.process_tasks import load_crawl_sources_url, scrape_source_job, insert_jobs_to_staging_layer, post_job_to_discord
+from tasks.tasks_group import itviec_pipeline
+from tasks.audit_tasks import task_failure_callback, task_success_callback
 
 logger = logging.getLogger(__name__)
 if not logger.handlers:
@@ -14,45 +15,24 @@ if not logger.handlers:
 default_args = {
     'owner': 'sonbao',
     'depends_on_past': False,
-    'start_date': datetime(2025, 3, 1),
+    'start_date': datetime(2025, 1, 1),
     'retries': 3,
-    'retry_delay': timedelta(seconds=30)
+    'retry_delay': timedelta(seconds=30),
+    'on_success_callback': task_success_callback,
+    'on_failure_callback': task_failure_callback
+
 }
 
 @dag(
     dag_id='itviec_data_pipeline',
     default_args=default_args,
-    schedule='@daily',
+    schedule_interval=timedelta(days=30),
     catchup=False,
     tags=['itviec_pipeline']
 )
 
-def itviec_pipeline():
+def _itviec_pipeline():
 
-    @task
-    def load_itviec_url():
-        return load_crawl_sources_url(source_crawl='itviec')
+    itviec_pipeline()
 
-    @task
-    def scrape_itviec_job(sources: dict):
-        return scrape_source_job(sources=sources, source_crawl='itviec')
-
-    @task
-    def insert_jobs_itviec(data):
-        insert_jobs_to_staging_layer(data=data, source_crawl='itviec')
-
-    @task
-    def post_job_to_discord_itviec():
-        post_job_to_discord(crawl_source='itviec')
-
-    itviec_sources = load_itviec_url()
-    job_data = scrape_itviec_job(itviec_sources)
-    insert = insert_jobs_itviec(job_data)
-    discord = post_job_to_discord_itviec()
-
-    insert >> discord
-
-    # insert >> bronze
-    # bronze >> [silver, discord] >> gold
-
-dag = itviec_pipeline()
+dag = _itviec_pipeline()
